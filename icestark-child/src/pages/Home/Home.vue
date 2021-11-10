4fb9bd2952389e8a0311762ebb550822db15eb8c
yangyiliang <yangyiliang@58.com>
2021-11-10 19:27:09 +0800

    child

---
+++ icestark-child/src/pages/Home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>小二：首页</div>
+    <router-link to="/detail">detail</router-link>
+    <br />
+    <router-link to="/list">list</router-link>
+    <br />
+    <el-alert
+      title="这是一个 Element 的组件"
+      type="success"
+    >
+    </el-alert>
+    <button @click="addToast">mount</button>
+    <div id='testmount'></div>
+    <toast></toast>
+  </div>
+</template>
+
+<script>
+import { appHistory } from '@ice/stark-app';
+import toast from './toast.vue'
+import Vue from 'vue';
+debugger
+export default {
+  components: {
+    toast
+  },
+  methods: {
+    handleClick: () => {
+      appHistory.push('/');
+    },
+    addToast() {
+      debugger
+      let a = new Vue(toast).$mount();
+      console.log(a.$el);
+      debugger
+    }
+  }
+}
+</script>
+
+
---
+++ icestark-child/src/pages/Home/toast.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class='12312'>12312312312313</div>
+</template>
+<script>
+export default {
+    data() {
+      return {}
+    }
+}
+</script>
